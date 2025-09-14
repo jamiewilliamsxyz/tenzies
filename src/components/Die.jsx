71baecf1173359ab4ee889b0757123f95e51d231
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-09-14 16:20:18 +0100

    Add aria accessibility to Die component

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -3,6 +3,10 @@ export const Die = (props) => {
     <button
       onClick={props.hold}
       className={`die ${props.isHeld && "die-held"}`}
+      aria-pressed={props.isHeld}
+      aria-label={`Die with value ${props.value}, ${
+        props.isHeld ? "Held" : "Not held"
+      }`}
     >
       {props.value}
     </button>
